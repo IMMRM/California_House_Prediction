da6254cfcd025d0f1e27a4879c0c96285692be1f
Immrm <mrmirdha@outlook.com>
2025-08-07 21:35:30 +0530

    AWS logging script added

---
+++ src/utils/log_db.py
@@ -0,0 +1,40 @@
+import psycopg2
+from psycopg2 import sql
+from datetime import datetime
+import json
+from src.constants import SECRET_PATH
+from src.utils.common import read_yaml
+
+DB_CONNECT=read_yaml(SECRET_PATH)
+
+# Database connection parameters (you can also load from a config file or env vars)
+DB_PARAMS = {
+    "dbname": DB_CONNECT['PostGres_DB']['DB_NAME'] ,
+    "user": DB_CONNECT['PostGres_DB']['DB_USER'],
+    "password": DB_CONNECT['PostGres_DB']['DB_PWD'],
+    "host": DB_CONNECT['PostGres_DB']['DB_HOST'],
+    "port": DB_CONNECT['PostGres_DB']['DB_PORT']
+}
+
+def init_db():
+    conn = psycopg2.connect(**DB_PARAMS)
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS predictions (
+            id SERIAL PRIMARY KEY,
+            input JSONB,
+            output REAL,
+            timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+def log_to_db(input_data: dict, prediction: float):
+    conn = psycopg2.connect(**DB_PARAMS)
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO predictions (input, output) VALUES (%s, %s)
+    """, (json.dumps(input_data), prediction))
+    conn.commit()
+    conn.close()
