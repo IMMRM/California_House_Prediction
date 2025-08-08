570e2e6a1e9c209148227d30bbeaf1b264f8ac59
Immrm <mrmirdha@outlook.com>
2025-08-08 10:50:45 +0530

    #fixed CI breaking issue with DB connect

--- src/utils/log_db.py
+++ src/utils/log_db.py
@@ -2,21 +2,31 @@ import psycopg2
 from psycopg2 import sql
 from datetime import datetime
 import json
-from src.constants import CONFIG_PATH
+from src.constants import SECRET_PATH
 from src.utils.common import read_yaml
+import os
 
-DB_CONNECT=read_yaml(CONFIG_PATH)
 
-# Database connection parameters (you can also load from a config file or env vars)
-DB_PARAMS = {
-    "dbname": 'postgres', #DB_CONNECT['PostGres_DB']['DB_NAME'] ,
-    "user": 'postgres', #DB_CONNECT['PostGres_DB']['DB_USER'],
-    "password": 'postgres' ,#DB_CONNECT['PostGres_DB']['DB_PWD'],
-    "host": 'database-1.c5eqggq6w2s1.ap-south-1.rds.amazonaws.com', #DB_CONNECT['PostGres_DB']['DB_HOST'],
-    "port": 5432
-}
+#Read secrets only if not running in CI
+if os.getenv("CI_ENV")!="true":
+    DB_CONNECT=read_yaml(SECRET_PATH)
+    # Database connection parameters (you can also load from a config file or env vars)
+    DB_PARAMS = {
+        "dbname": DB_CONNECT['PostGres_DB']['DB_NAME'] ,
+        "user": DB_CONNECT['PostGres_DB']['DB_USER'],
+        "password": DB_CONNECT['PostGres_DB']['DB_PWD'],
+        "host": DB_CONNECT['PostGres_DB']['DB_HOST'],
+        "port": DB_CONNECT['PostGres_DB']['DB_PORT']
+    }
+else:
+    DB_CONNECT=None
+
+
 
 def init_db():
+    if(os.getenv("CI_ENV")=="true"):
+        print("Running in CI - Skipping DB Init")
+        return
     conn = psycopg2.connect(**DB_PARAMS)
     cursor = conn.cursor()
     cursor.execute("""
@@ -31,6 +41,9 @@ def init_db():
     conn.close()
 
 def log_to_db(input_data: dict, prediction: float):
+    if(os.getenv("CI_ENV")=="true"):
+        print("Running in CI - skipping DB logging")
+        return
     conn = psycopg2.connect(**DB_PARAMS)
     cursor = conn.cursor()
     cursor.execute("""
